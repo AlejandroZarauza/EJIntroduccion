4fdf3d594369c30b4827e496c0eaec6fba4e4fb6
AlejandroZarauza <id00911083@usal.es>
2025-10-20 18:40:18 +0200

    Clases pelicula y director creadas y avances en el Main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,68 @@
+import modelo.*;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+    Scanner scan = new Scanner(System.in);
+    // Inicializar vector en un futuro
+
+    int opcion=0;
+
+        while (opcion != 5) {
+            System.out.println("MI VIDEOTECA");
+            System.out.println("--------------------------------------");
+            System.out.println("1) Nueva videoteca de películas");
+            System.out.println("2) Configurar velocidad de reproducción");
+            System.out.println("3) Añadir una nueva película a la vidioteca");
+            System.out.println("4) Mostrar información actual de las películas");
+            System.out.println("5) Salir (Se borrara toda la información)");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = scan.nextInt();
+            scan.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    
+                break;
+                case 2:
+                    
+                break;
+                case 3:
+                    
+                break;
+                case 4:
+                    
+                break;
+                case 5:
+                System.out.println("Saliendo de la vidioteca.");
+                break;
+                    
+                default:
+                System.out.println("Opción no valida");
+                
+            }
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            scan.close();
+        }
     }
-}
+
